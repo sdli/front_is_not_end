8340b743d46117b8c0de3c9b69c670dad1da24a0
Leo <lisd199001@gmail.com>
2019-03-25 15:52:42 +0800

    change docs

---
+++ docs/precache-manifest.7729a8949541926413dc23d041fc2ce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "42244d56bf99e9b5f7962456135316d1",
+    "url": "/static/media/style.42244d56.less"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6429eedb1b5902c3faad",
+    "url": "/static/js/main.6429eedb.chunk.js"
+  },
+  {
+    "revision": "98aa36ba185e711525f1",
+    "url": "/static/js/1.98aa36ba.chunk.js"
+  },
+  {
+    "revision": "6429eedb1b5902c3faad",
+    "url": "/static/css/main.95c9d8db.chunk.css"
+  },
+  {
+    "revision": "98aa36ba185e711525f1",
+    "url": "/static/css/1.24d29349.chunk.css"
+  },
+  {
+    "revision": "8b4b1525e3579fe237cb9264c80e83d3",
+    "url": "/index.html"
+  }
+];
